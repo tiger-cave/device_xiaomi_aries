c276d8b598cd0ae124915e484b2cd76ad7df1491
Bruno Martins <bgcngm@gmail.com>
2017-12-16 22:09:09 +0000

    aries: Add compatibility matrix
    Change-Id: Ibf6053f1a7e9df5384664379772cb8757e897622

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ USE_REDUCED_CJK_FONT_WEIGHTS := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8960
