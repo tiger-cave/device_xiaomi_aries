51cd7b8e82b17f6eafd6419d925d248687e8c60a
nasty007 <lazic.josip@gmail.com>
2018-01-02 21:13:45 +0100

    aries: Set up binderized camera HAL
    This mode stops the weird NULL deref crashes in camera stack when
    using CAMERA_MODULE_API_VERSION_2_4. We regain Torch support, and
    binderized operation was supposed to happen sooner or later.
    
    Change-Id: I07a9eff593b71566197c39d1b149696cf0463ad8
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- device.mk
+++ device.mk
@@ -85,6 +85,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.msm8960 \
     libmmcamera_interface2
