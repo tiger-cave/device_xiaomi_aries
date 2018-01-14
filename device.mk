343d05bd24f262081914255b3b1494ac374a26a8
Daniel Jarai <jaraidaniel@gmail.com>
2018-01-15 00:04:04 +0100

    aries: Binderize sensors HAL
    Change-Id: If25884155b718ab6256b661ad244fda005c1b3e5

--- device.mk
+++ device.mk
@@ -243,6 +243,7 @@ PRODUCT_COPY_FILES += \
 # Sensor
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     sensors.msm8960
 
 PRODUCT_COPY_FILES += \
