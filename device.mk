3e73483746c523544f6f4e22ccf476fbb33ec4cc
GuaiYiHu <guaiyihu@foxmail.com>
2018-10-12 00:00:38 +0800

    aries: keylayout: Map key as miui
    Change-Id: I0ba45f3d9e0b7c8ceb4db90e0343155f6669fc94

--- device.mk
+++ device.mk
@@ -21,7 +21,10 @@
 
 LOCAL_PATH := device/xiaomi/aries
 
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
