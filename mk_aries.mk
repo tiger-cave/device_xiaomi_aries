67f5541f59e8d50e36f2e49de8f528d5899f870e
GuaiYiHu <guaiyihu@foxmail.com>
2018-10-19 01:09:06 +0800

    aries: Remove device qcom common
    Change-Id: I418ef3a565ae6e9ea61eeff712b7e500d8beab6e

--- mk_aries.mk
+++ mk_aries.mk
@@ -19,9 +19,6 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Common QCOM configuration tools
-$(call inherit-product, device/qcom/common/Android.mk)
-
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/aries/device.mk)
 
