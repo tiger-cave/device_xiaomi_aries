72652af5b383e646ba4acbbc8442c3a819e53426
GuaiYiHu <guaiyihu@foxmail.com>
2019-05-09 09:54:57 +0800

    aries: Build Snap
    Change-Id: I87f0aa26efd04185548388520bc5c07cdce39282

--- device.mk
+++ device.mk
@@ -88,7 +88,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8960 \
-    libmmcamera_interface2
+    libmmcamera_interface2 \
+    Snap
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
