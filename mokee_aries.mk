c5fa5090cd482587e71144f0eaf5e78b94a236e9
GuaiYiHu <guaiyihu@foxmail.com>
2021-03-16 09:42:49 +0800

    aries: Switch to Android Go config file
    Change-Id: I248c6746ac19ac24ac8af9cb94ab9aa5f2495ce9

--- mokee_aries.mk
+++ mokee_aries.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/aries/aries.mk)
 
 # Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mokee/config/common_mini_phone.mk)
+$(call inherit-product, vendor/mokee/config/common_mini_go_phone.mk)
 
 PRODUCT_NAME := mokee_aries
 PRODUCT_CHARACTERISTICS := nosdcard
